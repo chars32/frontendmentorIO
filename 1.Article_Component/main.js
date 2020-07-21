0628b386f8ec160383c3d26c057e9bacc77a14bc
Carlos Garcia Castellanos <chars24@gmail.com>
2020-07-20 22:06:40 -0500

    2.2 Banner mobile ready 50%

--- 1.Article_Component/main.js
+++ 1.Article_Component/main.js
@@ -8,22 +8,23 @@ const buttonMessage = document.getElementsByClassName("button_message_show")[0];
 
 let toggleButton = false;
 
-shareButton.addEventListener("click", () => {
-  toggleButton = !toggleButton;
+// DESKTOP
+// shareButton.addEventListener("click", () => {
+//   toggleButton = !toggleButton;
 
-  if (toggleButton) {
-    shareButton.classList.add("change-color");
-    document.getElementsByClassName("button_message_show")[0].style.display =
-      "flex";
-    buttonMessage.classList.add("zoomIn");
-    buttonMessage.classList.remove("zoomOut");
-    arrowShare.setAttribute("src", "./images/icon-share-change.svg");
-    console.log("muestra boton");
-  } else {
-    shareButton.classList.remove("change-color");
-    buttonMessage.classList.remove("zoomIn");
-    buttonMessage.classList.add("zoomOut");
-    arrowShare.setAttribute("src", "./images/icon-share.svg");
-    console.log("esconde boton");
-  }
-});
+//   if (toggleButton) {
+//     shareButton.classList.add("change-color");
+//     document.getElementsByClassName("button_message_show")[0].style.display =
+//       "flex";
+//     buttonMessage.classList.add("zoomIn");
+//     buttonMessage.classList.remove("zoomOut");
+//     arrowShare.setAttribute("src", "./images/icon-share-change.svg");
+//     console.log("muestra boton");
+//   } else {
+//     shareButton.classList.remove("change-color");
+//     buttonMessage.classList.remove("zoomIn");
+//     buttonMessage.classList.add("zoomOut");
+//     arrowShare.setAttribute("src", "./images/icon-share.svg");
+//     console.log("esconde boton");
+//   }
+// });
