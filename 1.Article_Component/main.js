04dc5b80bbe2c766d29cf590907d204007311e44
Carlos Garcia Castellanos <chars24@gmail.com>
2020-07-24 13:26:14 -0500

    2.4 Listo el mobile.

--- 1.Article_Component/main.js
+++ 1.Article_Component/main.js
@@ -1,21 +1,39 @@
-const shareButton = document
-  .getElementsByClassName("share-button")[0]
-  .getElementsByTagName("figure")[0];
+// const shareButton = document
+//   .getElementsByClassName("share-button")[0]
+//   .getElementsByTagName("figure")[0];
 
-const arrowShare = shareButton.getElementsByTagName("img")[0];
+// const arrowShare = shareButton.getElementsByTagName("img")[0];
 
-const buttonMessage = document.getElementsByClassName("button_message_show")[0];
+// const buttonMessage = document.getElementsByClassName("button_message_show")[0];
 
-const textContainer = document.getElementsByClassName("text-container")[0];
+// const textContainer = document.getElementsByClassName("text-container")[0];
 
 let toggleButton = false;
 
 // MOBILE
-document.addEventListener("click", () => {
-  if (event.target == textContainer) {
-    console.log("iguales");
-  }
-});
+const buttonMessageShow = document.querySelector('.button_message_show');
+
+const arrowShareButton = document.querySelector('.share-button').querySelector('figure').querySelector('img')
+
+const arrowShareFigure = document.querySelector('.share-button').querySelector('figure')
+
+buttonMessageShow.style.display = "none";
+
+document.addEventListener('click', () => {
+  if(event.target == arrowShareButton){
+    toggleButton = !toggleButton
+
+    if (toggleButton == true){
+      buttonMessageShow.style.display = 'flex';
+      arrowShareFigure.style.zIndex = '99';
+      arrowShareFigure.style.backgroundColor = 'var(--body-txttwo-color)'
+      arrowShareButton.setAttribute("src", "./images/icon-share-change.svg")
+    }else if(toggleButton == false) {
+      buttonMessageShow.style.display = 'none';
+      arrowShareButton.setAttribute("src", "./images/icon-share.svg")
+    }
+  } 
+})
 
 // DESKTOP
 // shareButton.addEventListener("click", () => {
